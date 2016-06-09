d68423bb69b03f6ef3d5c5e1e18dca8d1d3c93b8
Brandon Jones <bdjones2410@gmail.com>
2016-06-09 10:57:23 -0400

    do you have the time?  I do

--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 // the clock should update every second or we will be late
 
 //make sure it gets put on the page
+
+function getTime(){
+  var time = new Date();
+
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+
+  return [hours, minutes, seconds];
+}
