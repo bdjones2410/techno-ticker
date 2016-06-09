545ca22657022c614bd68195c61076d5475da315
Nathan Hall <nathan.mh@gmail.com>
2016-06-09 11:27:49 -0400

    naynay plays nice with clocks and interval

--- main.js
+++ main.js
@@ -3,12 +3,36 @@
 //make sure it gets put on the page
 
 
-function getTime(){
-  var time = new Date();
 
-  var hours = time.getHours();
-  var minutes = time.getMinutes();
-  var seconds = time.getSeconds();
+$(document).ready(function() {
+  clockPage.init();
+})
 
-  return [hours, minutes, seconds];
+var clockPage = {
+
+  init: function() {
+    setInterval(function() {
+      clockPage.timing()
+    },1000)
+  },
+
+  timing: function() {
+    var $hour = $('#hours');
+    var $minute = $('#minutes');
+    var $seconds = $('#seconds');
+    var actualTime = clockPage.getTime();
+    $hour.html(actualTime[0] + ":")
+    $minute.html(actualTime[1] + ":")
+    $seconds.html(actualTime[2])
+  },
+  getTime: function() {
+
+    var time = new Date();
+
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+
+    return [hours, minutes, seconds];
+  }
 }
